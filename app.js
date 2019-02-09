2a1d8c1b94f93437b47f2222d491a6fa213a85d3
juan-colombo95 <42392347+juan-colombo95@users.noreply.github.com>
2019-02-09 11:54:32 -0300

    Add files via upload

--- app.js
+++ app.js
@@ -1,15 +1,30 @@
 document.getElementById('formtarea').addEventListener('submit',guardarTarea)
+    let title = document.getElementById('title').value
+ 
+function stoppedTyping(){
+    let title = document.getElementById('title')
+    if(title.value=="") { 
+            document.getElementById('btn-guardar').disabled = true; 
+        } else { 
+            document.getElementById('btn-guardar').disabled = false;
+        }
+}
 
 function guardarTarea(e){
     let title = document.getElementById('title').value
     let description = document.getElementById('description').value
     console.log(title+ description)
     
+   
+      
     
     const task = {
         title,
         description
+       
     }
+    
+    
     
    // localStorage.setItem('tasks',JSON.stringify(task))
     if(localStorage.getItem('tasks') === null){
@@ -27,6 +42,7 @@ function guardarTarea(e){
     getTasks()
 document.getElementById('formtarea').reset()
     e.preventDefault()
+    document.getElementById('btn-guardar').disabled = true; 
 }
 
 
@@ -40,16 +56,25 @@ function getTasks(){
     for(let i = 0 ;i<tasks.length;i++){
         let title = tasks[i].title
         let description = tasks[i].description
+        let checked = tasks[i].check
         taskview.innerHTML += `
 
 <div class="card pt-6 mt-3 mb-3">
            <div class ="card-body">
-            <p>${title} - ${description} </p>
-             <a class=" btn btn-danger" onclick="deleteTasks('${title}')">Delete</a>
+        
+ <div class="float-right custom-control custom-checkbox  ">
+  <input type="checkbox"    class="custom-control-input text-left" id="${title}">
+  <label class="custom-control-label"  for="${title}">Terminada</label>
+
+</div>
+<b>${title}</b><p>  ${description} </p>
+             <a class=" btn btn-danger" onclick="deleteTasks('${title}')">Borrar</a>
+
             </div>
              </div>`
     }
 }
+
 
 function deleteTasks(title){
     let tasks = JSON.parse(localStorage.getItem('tasks'))
